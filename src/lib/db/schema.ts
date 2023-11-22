8b052443e0e7c72dfe0cd0146ca8b83f37bdf20d
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-22 20:24:32 +0100

    config db schema and connection string

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,13 +1,21 @@
+import {pgTable,serial,text, varchar,timestamp, integer,pgEnum} from "drizzle-orm/pg-core"
 
-import {pgTable,serial,text} from "drizzle-orm/pg-core"
+export const userSystemEnum = pgEnum('user_system_enum',['system','user'])
 
-
-// export const chats = pgTable('chats', {
-//     id: serial('id').primaryKey(),
-//     pdfName: text('pdf_name').notNull(),
-// })
 const chats = pgTable('chats', {
     id: serial('id').primaryKey(),
     pdfName: text('pdf_name').notNull(),
     pdfUrl: text('pdf_url').notNull(),
+    createdAt: timestamp('createdAt').notNull().defaultNow(),
+    userId: varchar('user_id',{length:256}).notNull(),
+    fileKey: text('file_key').notNull(),
+
 })
+
+export const messages = pgTable("message", {
+    id: serial("id").primaryKey(),
+    chatId: integer('chat_id').references(()=>chats.id).notNull(),
+    content: text('content').notNull(),
+    createdAt: timestamp('createdAt').notNull().defaultNow(),
+    role: userSystemEnum('role').notNull()
+});

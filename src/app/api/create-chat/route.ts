5291ba40b74f13179a201857f492611d06353f74
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-23 23:32:10 +0100

    creating my backend api

---
+++ src/app/api/create-chat/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server"
+
+// /api/create-chat
+export async function POST(req:Request,res:Response){
+    try {
+        const body =await req.json()
+        const {file_key, file_name } = body
+    } catch (error) {
+        console.error()
+        return NextResponse.json({error:'Internal server error '}, {status:500})
+    }
+}

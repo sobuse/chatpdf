270b750aa5fca3b3e30e3aa5148893a1d01df2b2
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-25 18:24:16 +0100

    split and segment the pdf into smaller documents

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,7 @@
 import {PineconeClient} from "@pinecone-database/pinecone"
 import { downloadFroms3 } from "./s3-server";
 import {PDFLoader} from "langchain/document_loaders/fs/pdf"
+import { Document, RecursiveCharacterTextSplitter } from "@pinecone-database/doc-splitter";
 
 let pinecone: PineconeClient | null = null;
 
@@ -33,6 +34,30 @@ export async function loadS3intoPinecone(fileKey:string){
     }
     const loader = new PDFLoader(file_name);
     const pages = (await loader.load()) as PDFPage[];
-    //2. split and segment the pdf imto smaller documents
-    return pages;
+    //2. split and segment the pdf into smaller documents
+    
+    const documents = await Promise.all(pages.map(prepareDocument));
+
+    //
+}
+
+export const truncateStringByBite = (str: string , bytes: number) => {
+    const enc = new TextEncoder()
+    return new TextDecoder('utf-8').decode(enc.encode(str).slice(0,bytes))
+}
+
+async function prepareDocument(page: PDFPage){
+    let { pageContent,metadata} = page;
+    pageContent = pageContent.replace(/\n/g,'')
+    const splitter = new RecursiveCharacterTextSplitter();
+    const docs = await splitter.splitDocuments([
+        new Document({
+            pageContent,
+            metadata: {
+                pageNumber: metadata.loc.pageNumber,
+                text: truncateStringByBite(pageContent,36000)
+            }
+        })
+    ])
+    return docs
 }

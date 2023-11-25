2366f55a8b1fbe7c00158082ba1d7bbcd05f5ab5
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-25 09:07:33 +0100

    used toast package for alert

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,12 +1,16 @@
 import { NextResponse } from "next/server"
+import { toast } from "react-hot-toast";
 
 // /api/create-chat
 export async function POST(req:Request,res:Response){
     try {
         const body =await req.json()
         const {file_key, file_name } = body
+        console.log(file_key,file_name);
+        return NextResponse.json({message:"success"})
     } catch (error) {
         console.error()
+        
         return NextResponse.json({error:'Internal server error '}, {status:500})
     }
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from "@clerk/nextjs"
 import './globals.css'
 import Providers from '@/components/Providers'
+import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,9 @@ export default function RootLayout({
     <Providers>
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Toaster/> 
     </html>
+    
     </Providers>
     </ClerkProvider>
   )

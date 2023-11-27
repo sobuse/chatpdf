090dab0d42c5bb90dfb1b16f52a122192bd905ac
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-27 21:47:04 +0100

    Update embeddings.ts

--- src/lib/embeddings.ts
+++ src/lib/embeddings.ts
@@ -6,16 +6,36 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config)
 
-export async function getEmbeddings(text:string){
+
+export async function getEmbeddings(text: string) {
     try {
-        const response = await openai.createEmbedding({
-            model: 'text-embedding-ada-002',
-            input: text.replace(/\n/g,'')
-        })
-        const result = await response.json()
-        return result.data[0].embedding as number[]
+      const response = await openai.createEmbedding({
+        model: 'text-embedding-ada-002',
+        input: text.replace(/\n/g, ''),
+      });
+      const result = await response.json();
+  
+      if (result.data && result.data.length > 0 && result.data[0].embedding) {
+        return result.data[0].embedding as number[];
+      } else {
+        throw new Error('Embedding data is missing or undefined.');
+      }
     } catch (error) {
-        console.log('error calling openai embeddings api',error)
-        throw error;
+      console.log('error calling openai embeddings api', error);
+      throw error;
     }
-}
+  }
+
+// export async function getEmbeddings(text:string){
+//     try {
+//         const response = await openai.createEmbedding({
+//             model: 'text-embedding-ada-002',
+//             input: text.replace(/\n/g,'')
+//         })
+//         const result = await response.json()
+//         return result.data[0].embedding as number[]
+//     } catch (error) {
+//         console.log('error calling openai embeddings api',error)
+//         throw error;
+//     }
+// }

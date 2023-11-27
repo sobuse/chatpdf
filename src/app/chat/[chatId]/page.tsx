4ef37f9470fbf46f78a7f36002c19837e8f5110c
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-11-27 23:10:40 +0100

    Create page.tsx

---
+++ src/app/chat/[chatId]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+    params:{
+        chatId: string
+    }
+}
+
+const chatPage = ({params:{chatId}}: Props) => {
+  return 
+    <div>chatPage</div>
+  
+}
+
+export default chatPage

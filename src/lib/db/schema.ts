1ddb487a7d653f7ca020e00ce2f3322fe14a6917
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-12-06 16:25:11 +0100

    added schema for stripe integration

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -10,7 +10,9 @@ export const chats = pgTable('chats', {
     userId: varchar('user_id',{length:256}).notNull(),
     fileKey: text('file_key').notNull(),
 
-})
+});
+
+export type DrizzleChat = typeof chats.$inferInsert;
 
 export const messages = pgTable("message", {
     id: serial("id").primaryKey(),
@@ -18,4 +20,14 @@ export const messages = pgTable("message", {
     content: text('content').notNull(),
     createdAt: timestamp('createdAt').notNull().defaultNow(),
     role: userSystemEnum('role').notNull()
+});
+
+export const userSubsription = pgTable('user_subscription',{
+    id:serial('id').primaryKey(),
+    userId: varchar('user_id', {length:256}).notNull().unique(),
+    stripeCustomerId: varchar('stripe_customer_id',{length:256}).notNull().unique(),
+    stripeSubscriptionId: varchar('stripe_subscription_id', {length:256}).unique(),
+    stripePriceId: varchar('stripe_price_id', {length:256}),
+    stripeCurrentPeriodEnd: timestamp('stripe_current_period_end'),
+
 });
